e0f48cefdef6b93ace2dd13c7a734d40ff835b94
Omen1984 <ivan.plehanov42@yandex.ru>
2022-11-19 13:36:57 +0500

    Initial commit. Task 1: parse CSV to City

---
+++ src/main/java/ru/sber/directory/model/City.java
@@ -0,0 +1,71 @@
+package ru.sber.directory.model;
+
+import java.util.Objects;
+
+public class City {
+
+    private final long id;
+    private final String name;
+    private final String region;
+    private final String district;
+    private final int population;
+    private final String foundation;
+
+    public City(long id, String name, String region, String district, int population, String foundation) {
+        this.id = id;
+        this.name = name;
+        this.region = region;
+        this.district = district;
+        this.population = population;
+        this.foundation = foundation;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public String getFoundation() {
+        return foundation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof City)) return false;
+        City city = (City) o;
+        return id == city.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", region='" + region + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                ", foundation=" + foundation +
+                '}';
+    }
+}
